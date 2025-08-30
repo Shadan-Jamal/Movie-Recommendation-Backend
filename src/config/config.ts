79533b51a559bc5ed328922d6c5ff0c578d72036
Shadan Jamal <shadowfrost1470@gmail.com>
2025-08-30 13:11:31 +0530

    production change

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,11 +5,16 @@ dotenv.config()
 interface Config {
     port: number;
     nodeEnv: string;
+    frontendUrl: string;
   }
   
+const frontendUrl = process.env.NODE_ENV === "development" ? process.env.FRONTEND_URL_DEV : process.env.FRONTEND_URL_PROD;
+console.log(process.env.PORT)
   const config: Config = {
-    port: Number(process.env.PORT) || 3000,
+    port: Number(process.env.PORT) || 3001,
     nodeEnv: process.env.NODE_ENV || 'development',
+    frontendUrl
   };
   
+  console.log("Configuration Loaded:", config);
 export default config;
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response} from "express";
+import express from "express";
 import descriptionRouter from "./routes/description";
 import emotionRouter from "./routes/emotion"
 import config from "./config/config"
@@ -8,34 +8,18 @@ import { PineconeService } from "./services/PineconeService";
 
 const app = express()
 
-// Determine frontend URL based on environment
-const frontendUrl =
-    process.env.NODE_ENV === "production"
-        ? process.env.FRONTEND_URL_PROD
-        : process.env.FRONTEND_URL_DEV;
-console.log(process.env.NODE_ENV)
-console.log(frontendUrl)
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`)
 // middleware
 app.use(express.json())
 app.use(cors({
-    origin: frontendUrl
+    origin: config.frontendUrl
 }))
 
 export const text_classification_pipeline = new EmotionClassifier()
 export const pinecone_service = new PineconeService()
 
-app.get("/", (req : Request, res : Response) => {
-    console.log("Hi world")
-    res.send("Hello, World!")
-})
 app.use("/description", descriptionRouter);
 app.use("/emotion", emotionRouter)
-
-// Error handling middleware
-app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
-    console.error(err.stack);
-    res.status(500).json({ error: 'Something broke!' });
-});
 
 app.listen(config.port, () => {
     console.log(`Listening on port: ${config.port}`)
